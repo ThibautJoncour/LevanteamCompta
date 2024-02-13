9e657a028afd502125b82da5c981b0899092dde3
ThibautJoncour <96741150+ThibautJoncour@users.noreply.github.com>
2024-02-13 18:41:36 +0100

    Delete project directory

--- project/blog/serializers.py
+++
@@ -1,8 +0,0 @@
-# Dans serializers.py
-from rest_framework import serializers
-from .models import VotreModele
-
-class VotreModeleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VotreModele
-        fields = '__all__'
--- project/project/static/java.js
+++
@@ -1,8 +0,0 @@
-
-
-//"document.write('test');"
-
-//"document.body.append('test');"
-
-//"let h1 = document.querySelector('h1')"
-//"h1.append('test')"
